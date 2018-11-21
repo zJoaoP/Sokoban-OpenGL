e8f9b29dab44c7a9221c0c129d9dde6446bd4857
João Pedro <contato.joaocomp@gmail.com>
2018-11-21 20:18:34 -0300

    Desenhos e camera adicionados

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 #define SCREEN_WIDTH 	680
 #define SCREEN_HEIGHT 	440
-#define DEBUG			true
+#define DEBUG			(true)
 
 class Point{
 	private:
@@ -23,6 +23,18 @@ class Point{
 			this->y = y;
 			this->z = z;
 		}
+
+		float getX(){
+			return this->x;
+		}
+
+		float getY(){
+			return this->y;
+		}
+
+		float getZ(){
+			return this->z;
+		}
 };
 
 class Object{
@@ -34,60 +46,82 @@ class Object{
 			this->position = Point(x, y, z);
 		}
 
-		virtual void draw(){
-			if(DEBUG)
-				printf("[Object] Virtual draw method called.\n");
+		virtual void draw() {}
+
+		void drawCubeOnCurrentPosition(float size){
+			glPushMatrix();
+				glMatrixMode(GL_MODELVIEW);
+				glLoadIdentity();
+
+				glTranslatef(position.getX(), position.getY(), position.getZ());
+				glutSolidCube(size);
+			glPopMatrix();
+		}
+
+		void drawSphereOnCurrentPosition(float radius){
+			glPushMatrix();
+				glMatrixMode(GL_MODELVIEW);
+				glLoadIdentity();
+
+				glTranslatef(position.getX(), position.getY(), position.getZ());
+				glutSolidSphere(radius, 20, 20);
+			glPopMatrix();
+		}
+
+		Point getPosition(){
+			return position;
 		}
 };
 
 class Wall : public Object{
 	public:
-		Wall(float x, float y, float z) : Object(x, y, z){
-			if(DEBUG)
-				printf("[Object] Criando uma parede em (%f, %f, %f).\n", x, y, z);
-		}
+		Wall(float x, float y, float z) : Object(x, y, z) {}
 
 		void draw(){
-			if(DEBUG)
-				printf("[Wall] Desenhando uma parede!\n");
+			glColor3f(0.41, 0.41, 0.41);
+			this->drawCubeOnCurrentPosition(1.0);
 		}
 };
 
 class Ground : public Object{
 	public:
-		Ground(float x, float y, float z) : Object(x, y - 1.0, z){
-			if(DEBUG)
-				printf("[Object] Criando um chão em (%f, %f, %f).\n", x, y - 1.0, z);	
+		Ground(float x, float y, float z) : Object(x, y - 1.0, z) {}
+
+		void draw(){
+			glColor3f(0.19, 0.19, 0.19);
+			this->drawCubeOnCurrentPosition(1.0);
 		}
 };
 
 class Box : public Object{
 	public:
-		Box(float x, float y, float z) : Object(x, y, z){
-			if(DEBUG)
-				printf("[Object] Criando uma caixa em (%f, %f, %f).\n", x, y - 1.0, z);		
-		}	
+		Box(float x, float y, float z) : Object(x, y, z) {}
+
+		void draw(){
+			glColor3f(0.87, 0.72, 0.52);
+			this->drawCubeOnCurrentPosition(1.0);
+		}
 };
 
 class Player : public Object{
 public:
-		Player(float x, float y, float z) : Object(x, y, z){
-			if(DEBUG)
-				printf("[Object] Criando o jogador em (%f, %f, %f).\n", x, y - 1.0, z);		
+		Player(float x, float y, float z) : Object(x, y, z) {}
+
+		void draw(){
+			glColor3f(0, 0, 0);
+			this->drawSphereOnCurrentPosition(0.5);
 		}
 };
 
 class Target : public Object{
 public:
-		Target(float x, float y, float z) : Object(x, y, z){
-			if(DEBUG)
-				printf("[Object] Criando objetivo em (%f, %f, %f).\n", x, y - 1.0, z);		
-		}	
+		Target(float x, float y, float z) : Object(x, y, z) {}
 };
 
 class Scene{
 	private:
 		vector <Object*> objects;
+		Object *player;
 		static Scene* instance;
 
 		Scene(){}
@@ -121,11 +155,15 @@ class Scene{
 							objects.push_back(new Wall(x, y, z));
 						else if(objectType == 2)
 							objects.push_back(new Box(x, y, z));
-						else if(objectType == 3)
-							objects.push_back(new Player(x, y, z));
+						else if(objectType == 3){
+							player = new Player(x, y, z);
+							objects.push_back(new Ground(x, y, z));
+							objects.push_back(player);
+						}
 						else if(objectType == 4)
 							objects.push_back(new Ground(x, y, z));
-						
+						else
+							objects.push_back(new Ground(x, y, z));						
 					}
 				}
 				fclose(file);
@@ -136,25 +174,107 @@ class Scene{
 			for(int i = 0; i < objects.size(); i++)
 				objects[i]->draw();
 		}
+
+		Point getPlayerPosition(){
+			return player->getPosition();
+		}
+
+		Point getCameraPositionByReference(int cameraReference){
+			Point playerPosition = getPlayerPosition();
+			float cameraX = 0.0, cameraY = 12.9, cameraZ = 0.0;
+			float defaultOffset = 17.6;
+
+			if(cameraReference == 0){
+				cameraX = playerPosition.getX();
+				cameraZ = playerPosition.getZ() - defaultOffset;
+			}
+			else if(cameraReference == 1){
+				cameraX = playerPosition.getX() + defaultOffset;
+				cameraZ = playerPosition.getZ();
+			}
+			else if(cameraReference == 2){
+				cameraX = playerPosition.getX();
+				cameraZ = playerPosition.getZ() + defaultOffset;
+			}
+			else{
+				cameraX = playerPosition.getX() - defaultOffset;
+				cameraZ = playerPosition.getZ();
+			}
+			return Point(cameraX, cameraY, cameraZ);
+		}
+
+		Point getFrontPointByReference(int cameraReference){
+			Point playerPosition = getPlayerPosition();
+			float cameraX = 0.0, cameraY = 0.5, cameraZ = 0.0;
+			if(cameraReference == 0){
+				cameraX = playerPosition.getX();
+				cameraZ = playerPosition.getZ() + 1.0;
+			}
+			else if(cameraReference == 1){
+				cameraX = playerPosition.getX() - 1.0;
+				cameraZ = playerPosition.getZ();
+			}
+			else if(cameraReference == 2){
+				cameraX = playerPosition.getX();
+				cameraZ = playerPosition.getZ() - 1.0;
+			}
+			else{
+				cameraX = playerPosition.getX() + 1.0;
+				cameraZ = playerPosition.getZ();
+			}
+			return Point(cameraX, cameraY, cameraZ);
+		}
 };
 
 Scene* Scene::instance = 0;
+int cameraReference = 1;
+
+void setupCamera(){
+	Scene *scene = Scene::getInstance();
+	Point lookPointPosition = scene->getFrontPointByReference(cameraReference);
+	Point cameraPosition = scene->getCameraPositionByReference(cameraReference);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(10.0f, 16.0/9.0, 0.1, 100.0);
+	gluLookAt(cameraPosition.getX(), cameraPosition.getY(), cameraPosition.getZ(), lookPointPosition.getX(), lookPointPosition.getY(), lookPointPosition.getZ(), 0, 1, 0);
+}
 
 void draw(){
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	setupCamera();
 	
 	Scene *scene = Scene::getInstance();
 	scene->draw();
 
-	glFlush();
+	glutSwapBuffers();;
 }
 
 void initScene(){
 	glMatrixMode(GL_PROJECTION);
 
-	glOrtho(-5.0, 20.0, -5.0, 20.0, -5.0, 20.0);
+	glOrtho(-100.0, 100.0, -100.0, 100.0, -100.0, 100.0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void specialInput(int key, int x, int y){
+	switch(key){
+		case GLUT_KEY_LEFT:{
+			cameraReference -= 1;
+			glutPostRedisplay();
+			break;
+		}
+		case GLUT_KEY_RIGHT:{
+			cameraReference += 1;
+			glutPostRedisplay();
+			break;
+		}
+	}
+	cameraReference = (cameraReference < 0) ? 3 : (cameraReference % 4);
 }
 
 int main(int argc, char *argv[]){
@@ -175,6 +295,7 @@ int main(int argc, char *argv[]){
 	glutInitWindowSize(680, 440);
 	glutCreateWindow("Sokoban - OpenGL");
 	
+	glutSpecialFunc(specialInput);
 	glutDisplayFunc(draw);
 	
 	initScene();
